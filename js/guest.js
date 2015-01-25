c4f4b211dfd827be553823a4202d3e14a1ae78cd
Dan Bolivar <danbo314@gmail.com>
2015-01-25 02:23:01 -0500

    revamp guest list page

---
+++ js/guest.js
@@ -0,0 +1,65 @@
+$(function() {
+
+    // initial load of all guests
+	refreshGuests(false);
+
+    $("#tableCont").css({ "max-height": $("#guestTable").height()-$("#yearTabs").height()+"px" });
+
+    $(window).resize(function () {
+        $("#tableCont").css({ "max-height": $("#guestTable").height()-$("#yearTabs").height()+"px" });
+    });
+
+    // load all available graduation years
+    getAllYears(function(years) {
+        var i,
+            ylen = years.length,
+            $currTab,
+            cc = "green";
+
+        for (i = 0; i < ylen; i++) {
+            cc = (cc === "green") ? "blue" : "green";
+            $('#yearTabs').append("<span class='tab "+cc+"'>"+years[i]+"</span>");
+        }
+
+        var filter;
+
+        $("span.tab").on("click", function () {
+            $(".tab.selected").removeClass("selected");
+            $(this).addClass("selected");
+
+            filter = ($(this).html() === "All") ? false : { gradYear: parseInt($(this).html()) };
+
+            refreshGuests(filter);
+        });
+    });
+
+});
+
+function refreshGuests(filter) {
+    getRegistrations(filter, function(data) {
+        var $table = $('#tableCont > table'),
+            i,
+            dlen = data.length,
+            attClass,
+            gy;
+
+        $table.find("tr:not(.headers)").remove();
+
+        for (i = 0; i< dlen; i++) {
+            //data[i].attending = Math.floor(Math.random() * 10) + 1;
+            if (data[i].attending < 5) {
+                attClass = "down";
+            }
+            else if (data[i].attending < 8) {
+                attClass = "mid";
+            }
+            else {
+                attClass = "up";
+            }
+
+            gy = !filter ? "<span>'"+data[i].gradYear.toString().substring(2)+"</span>" : "";
+
+            $table.append("<tr><td>"+data[i].fullName+gy+"</td><td>"+data[i].nickName+"</td><td class='"+attClass+"'>"+data[i].attending+"</td></tr>");
+        }
+    });
+}
--- js/landing.js
+++ js/landing.js
@@ -19,14 +19,4 @@ $(function() {
         $(".pimg:not(#fourBG)").height($(window).height());
         $("#fourBG").height($(window).height()-100);
     });
-
-    var oldT;
-
-    $("#guestList").hover(function () {
-        oldT = $(this).text();
-        $(this).text("Coming Soon");
-    },
-    function () {
-        $(this).text(oldT);
-    });
 });
